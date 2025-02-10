2e93a747cfbe4431b597895fb2865dd2a801d073
Koleris <59147214+koleriss@users.noreply.github.com>
2025-02-10 15:03:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ pkg update && pkg upgrade -y
 # Menginstal paket yang diperlukan di Termux
 pkg install libexpat openssl python -y
 pkg install ffmpeg -y
-pip install colorama -y
+pip install colorama
 
 # Menginstal dependensi Python
 pip install -U "yt-dlp[default]"
